6cb4d351c7eed8fea20b96b488123d416d272286
akanksha0209 <akankshaapai.cs20@rvce.edu.in>
2023-12-01 10:48:07 +0530

    initial

---
+++ google-meet-clone-master/src/server/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "1:682570244089:web:ee03c941426078cc5de728", // Add API Key
+  databaseURL:"https://myapp-790b6-default-rtdb.firebaseio.com/" // Add databaseURL
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase;
+
+var firepadRef = firebase.database().ref();
+
+export const userName = prompt("What's your name?");
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
+
+if (roomId) {
+  firepadRef = firepadRef.child(roomId);
+} else {
+  firepadRef = firepadRef.push();
+  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
+}
+
+export default firepadRef;
